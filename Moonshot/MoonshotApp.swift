6cca1afde368d0167dacc19d10a25180c617e683
Octavius Bowman <octaviusbowman@gmail.com>
2021-11-05 17:13:12 -0400

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Octavius Bowman on 11/5/21.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
